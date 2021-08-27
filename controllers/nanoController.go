8685cf25f13d8c373f89dfda707f5d7598df9ede
septiansah nur aziz <septiansahnuraziz@gmail.com>
2021-08-27 14:29:07 +0700

    fixing func printing

--- controllers/nanoController.go
+++ controllers/nanoController.go
@@ -2,9 +2,7 @@ package controllers
 
 import (
 	"encoding/json"
-	"flag"
 	"fmt"
-	"image"
 	"log"
 	"nano-service/config"
 	handler "nano-service/handlers"
@@ -20,7 +18,6 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/knq/escpos"
-	"github.com/knq/escpos/raster"
 )
 
 type NanoRepo struct {
@@ -159,33 +156,8 @@ func (n *NanoRepo) CekAntrian(c *gin.Context) {
 
 }
 
-var (
-	// lpDev     = flag.String("p", , "Printer dev file")
-	imgPath   = flag.String("i", "./assets/img_logo.png", "Input image")
-	threshold = flag.Float64("t", 0.5, "Black/white threshold")
-	align     = flag.String("a", "center", "Alignment (left, center, right)")
-	doCut     = flag.Bool("c", false, "Cut after print")
-	maxWidth  = flag.Int("printer-max-width", 512, "Printer max width in pixels")
-)
-
 func PrintTicket(noAntrian, pelayanan, tgl, jam string) {
-	
 
-	flag.Parse()
-
-	imgFile, err := os.Open(*imgPath)
-	if err != nil {
-		log.Println("tes")
-		log.Fatal(err)
-	}
-
-	img, imgFormat, err := image.Decode(imgFile)
-	imgFile.Close()
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-
-	log.Print("Loaded image, format: ", imgFormat)
 	// log.Println("INI PAtH ", *lpDev)
 	f, err := os.OpenFile(os.Getenv("PRINTER_ADDRESSS"), os.O_RDWR, 0)
 	if err != nil {
@@ -199,12 +171,6 @@ func PrintTicket(noAntrian, pelayanan, tgl, jam string) {
 
 	ep.SetAlign("center")
 
-	rasterConv := &raster.Converter{
-		MaxWidth:  *maxWidth,
-		Threshold: *threshold,
-	}
-
-	rasterConv.Print(img, ep)
 	ep.Formfeed()
 	ep.Formfeed()
 
@@ -217,7 +183,7 @@ func PrintTicket(noAntrian, pelayanan, tgl, jam string) {
 	ep.SetFontSize(2, 3)
 	ep.SetFont("B")
 	ep.Write("Antrian")
-	ep.Write("  ")
+	ep.Formfeed()
 	ep.Formfeed()
 
 	ep.SetFontSize(1, 2)
@@ -235,53 +201,88 @@ func PrintTicket(noAntrian, pelayanan, tgl, jam string) {
 	ep.SetFontSize(1, 2)
 	ep.Write("Tanggal " + tgl + " | Waktu: " + jam)
 	ep.FormfeedN(3)
-	if *doCut {
-		ep.Cut()
-	}
+
+	ep.Cut()
 	ep.End()
 }
 
-// func (n *NanoRepo) DownloadPdf(c *gin.Context) {
-// 	c.Header("Access-Control-Allow-Headers", "Content-type")
-// 	c.Header("Access-Control-Allow-Method", "POST, GET, OPTIONS, PUT, DELETE")
-// 	c.Header("Access-Control-Allow-Origin", "*")
-// 	var responses models.ResponseGA
-// 	id := c.Query("id")
-// 	i, _ := strconv.Atoi(id)
+// var (
+// 	// lpDev     = flag.String("p", , "Printer dev file")
+// 	imgPath   = flag.String("i", "./assets/img_logo.png", "Input image")
+// 	threshold = flag.Float64("t", 0.5, "Black/white threshold")
+// 	align     = flag.String("a", "center", "Alignment (left, center, right)")
+// 	doCut     = flag.Bool("c", false, "Cut after print")
+// 	maxWidth  = flag.Int("printer-max-width", 512, "Printer max width in pixels")
+// )
 
-// 	payload, err := n.repo.GetAntrian(i)
+// func PrintTicket(noAntrian, pelayanan, tgl, jam string) {
 
+// 	flag.Parse()
+
+// 	imgFile, err := os.Open(*imgPath)
 // 	if err != nil {
-// 		c.AbortWithStatusJSON(400, handler.ErrorHandler(400, 404, err.Error()))
-// 		log.Panicln(err)
-// 		return
-// 	}
-// 	errPdf := GeneratePDF(payload)
-// 	if errPdf != nil {
-// 		c.AbortWithStatusJSON(400, handler.ErrorHandler(400, 404, err.Error()))
-// 		log.Panicln(err)
-// 		return
+// 		log.Println("tes")
+// 		log.Fatal(err)
 // 	}
 
-// 	responses.Status = 200
-// 	responses.Message = "Success"
-// 	responses.Data = payload
-// 	c.Header("Content-Type", "application/json")
-// 	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-// 	json.NewEncoder(c.Writer).Encode(responses)
-// }
-
-// func GeneratePDF(m models.GetAntrian) error{
-
-// 	pdf := gofpdf.New("L", "mm", "A4", "")
-// 	pdf.AddPage()
-// 	pdf.SetFont("Arial", "B", 16)
-// 	// pdf.Text(90, 30, m.Pelayanan)
-// 	pdf.CellFormat(250, 7, "judul "+ m.Pelayanan, "0", 0, "CM", false, 0, "")
-
-// 	errPdf := pdf.OutputFileAndClose("./documents/file2.pdf")
-// 	if errPdf != nil {
-// 		log.Println("ERROR", errPdf.Error())
+// 	img, imgFormat, err := image.Decode(imgFile)
+// 	imgFile.Close()
+// 	if err != nil {
+// 		log.Fatal(err.Error())
 // 	}
-// 	return nil
+
+// 	log.Print("Loaded image, format: ", imgFormat)
+// 	// log.Println("INI PAtH ", *lpDev)
+// 	f, err := os.OpenFile(os.Getenv("PRINTER_ADDRESSS"), os.O_RDWR, 0)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+// 	defer f.Close()
+
+// 	ep := escpos.New(f)
+
+// 	ep.Init()
+
+// 	ep.SetAlign("center")
+
+// 	rasterConv := &raster.Converter{
+// 		MaxWidth:  *maxWidth,
+// 		Threshold: *threshold,
+// 	}
+
+// 	rasterConv.Print(img, ep)
+// 	ep.Formfeed()
+// 	ep.Formfeed()
+
+// 	ep.SetSmooth(1)
+// 	ep.SetFontSize(1, 2)
+// 	ep.SetFont("A")
+// 	ep.Write("Pengadilan Negeri Jakarta Selatan")
+// 	ep.Formfeed()
+
+// 	ep.SetFontSize(2, 3)
+// 	ep.SetFont("B")
+// 	ep.Write("Antrian")
+// 	ep.Write("  ")
+// 	ep.Formfeed()
+
+// 	ep.SetFontSize(1, 2)
+// 	ep.Write("Loket " + pelayanan)
+// 	ep.Write("  ")
+// 	ep.Formfeed()
+// 	ep.Formfeed()
+
+// 	ep.SetFontSize(4, 5)
+// 	ep.Write(noAntrian)
+// 	ep.Write("  ")
+// 	ep.Formfeed()
+// 	ep.Formfeed()
+
+// 	ep.SetFontSize(1, 2)
+// 	ep.Write("Tanggal " + tgl + " | Waktu: " + jam)
+// 	ep.FormfeedN(3)
+// 	if *doCut {
+// 		ep.Cut()
+// 	}
+// 	ep.End()
 // }
