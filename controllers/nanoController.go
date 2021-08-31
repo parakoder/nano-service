c802a3a5fe4a599229c80915ec6af63fe77662f9
septiansah nur aziz <septiansahnuraziz@gmail.com>
2021-08-31 23:51:28 +0700

    change param jam kedatnagan

--- controllers/nanoController.go
+++ controllers/nanoController.go
@@ -2,7 +2,9 @@ package controllers
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
+	"image"
 	"log"
 	"nano-service/config"
 	handler "nano-service/handlers"
@@ -18,6 +20,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/knq/escpos"
+	"github.com/knq/escpos/raster"
 )
 
 type NanoRepo struct {
@@ -206,14 +209,59 @@ func PrintTicket(noAntrian, pelayanan, tgl, jam string) {
 	ep.End()
 }
 
-// var (
-// 	// lpDev     = flag.String("p", , "Printer dev file")
-// 	imgPath   = flag.String("i", "./assets/img_logo.png", "Input image")
-// 	threshold = flag.Float64("t", 0.5, "Black/white threshold")
-// 	align     = flag.String("a", "center", "Alignment (left, center, right)")
-// 	doCut     = flag.Bool("c", false, "Cut after print")
-// 	maxWidth  = flag.Int("printer-max-width", 512, "Printer max width in pixels")
-// )
+var (
+	// lpDev     = flag.String("p", , "Printer dev file")
+	imgPath   = flag.String("i", "./assets/img_logo.png", "Input image")
+	threshold = flag.Float64("t", 0.5, "Black/white threshold")
+	align     = flag.String("a", "center", "Alignment (left, center, right)")
+	doCut     = flag.Bool("c", false, "Cut after print")
+	maxWidth  = flag.Int("printer-max-width", 512, "Printer max width in pixels")
+)
+
+func PrintTicket() {
+	flag.Parse()
+
+	imgFile, err := os.Open(*imgPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	img, imgFormat, err := image.Decode(imgFile)
+	imgFile.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Print("Loaded image, format: ", imgFormat)
+
+	// ----------------------------------------------------------------------
+
+	f, err := os.OpenFile(os.Getenv("PRINTER_ADDRESSS"), os.O_RDWR, 0)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+	// log.Print(*lpDev, " open.")
+
+	ep := escpos.New(f)
+
+	ep.Init()
+
+	ep.SetAlign(*align)
+
+	rasterConv := &raster.Converter{
+		MaxWidth:  *maxWidth,
+		Threshold: *threshold,
+	}
+
+	rasterConv.Print(img, ep)
+
+	if *doCut {
+		ep.Cut()
+	}
+	ep.End()
+}
 
 // func PrintTicket(noAntrian, pelayanan, tgl, jam string) {
 
